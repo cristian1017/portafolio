ab28bcdbfdc6a91a2e790aec398f29855cf8d316
cristian1017 <cristianmauriciogutierrez1998@gmail.com>
2021-09-28 13:15:05 -0500

    agregando otro proyecto heroes App

--- src/components/slider/Slides.jsx
+++ src/components/slider/Slides.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import './Slider.css';
 import pos from '../../img/portada.png';
-import home from '../../img/homeCalendar.png'
+import home from '../../img/homeCalendar.png';
+import heroes from '../../img/heroesApp.png'
+
 
 
 const slideInfo = [
@@ -16,6 +18,13 @@ const slideInfo = [
         alt: "Proyecto 2",
         desc: "Calendario App",
         git: "MERN-Calendar-Front"
+    },
+    {
+        src: heroes,
+        alt: "Proyecto 3",
+        desc: "Heroes App",
+        git: "Heroes-App",
+        deploy: "https://cristian1017.github.io/Heroes-App/"
     },
    
 ]
